b08287ac86a3c12cd10fb70896abc8c1d136cc5b
ploploplop <klouseliptun@mail.ru>
2025-02-05 22:36:44 +0400

    Update steam.py

--- steam.py
+++ steam.py
@@ -25,4 +25,25 @@ def save_api_user_id(message):
         else:
             bot.reply_to(message, 'Отправьте два значения: API ключ и User ID.')
 
+@bot.message_handler(message=['get_sales'])
+def get_sales(message):
+    chat_id = message.chat.id
+    if chat_id not in users_data:
+        bot.reply_to(message, 'Сначала отправьте два значения: API ключ и User ID')
+        return
+
+    user_data = users_data[chat_id]
+    API_KEY = user_data['API_KEY']
+    User_ID = user_data['User_ID']
+
+    sales = get_sales(API_KEY, User_ID)
+    if sales:
+        sale_message = "Вот текущие скидки Steam игр, на которые вы подписались.:\n\n"
+        for sale in sales:
+            sale_message += f"{sale['name']}: {sale['discount']}% off, now {sale['final_price']}\n"
+        bot.reply_to(message, sale_message)
+    else:
+        bot.reply_to(message, 'Скидок не найдено')
+
+
 bot.polling(none_stop=True)
